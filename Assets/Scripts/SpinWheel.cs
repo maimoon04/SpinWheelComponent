4f5f398d86bac9677794db5220e77dd0110b9b8d
maimoon04 <moonishtiaq5@gmail.com>
2024-04-03 00:43:47 +0500

    spawned tiles in spinwheel

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SpinWheel : MonoBehaviour
+using System;
+using Sirenix.OdinInspector;
+[CreateAssetMenu(fileName = "SpinWheel", menuName = "SpinWheel/CreateSpinWheel")]
+public class SpinWheel : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform parent;
+    [SerializeField] private GameObject WheelTile;
+    [SerializeField] private WheelDataContainer wheelData;
+
+    [SerializeField] private List<GameObject> wheeltilelist;
+    public int numberOfTiles ;
+    public float radius ;
+
+
+    [Button]
+    void MakeSpinWheel()
     {
+        float angleStep = 360f / numberOfTiles;
         
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            float angle = i * angleStep;
+
+            Vector3 tilePosition = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad) * radius, Mathf.Cos(angle * Mathf.Deg2Rad) * radius, 0f);
+            GameObject tile = Instantiate(WheelTile,parent);
+            tile.transform.rotation = Quaternion.Euler(0f, 0f, -angle);
+            wheeltilelist.Add(tile);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
